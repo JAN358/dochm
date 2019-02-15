3cbf1324f313296e8c149641861935a30c323d01
JAN358 <zhannur.gareev@headmade.pro>
2019-02-15 13:27:50 +0300

    start proj

---
+++ sait/src/index.js
@@ -0,0 +1,9 @@
+import './css/main.css'
+import '../scss/app.scss'
+//import '../node_modules/jquery/dist/jquery'
+//import '../node_modules/what-input/dist/what-input'
+//import '../node_modules/foundation-sites/dist/js/foundation'
+//import './js/common'
+//import '../node_modules/bootstrap/dist/js/bootstrap.min.js'
+
+//window.Vue = require('vue')
